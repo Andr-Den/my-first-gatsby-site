a50a4d114427690cc7d51e5295b03ad95076aa2a
Andrey Denisov <telcontar2012@yandex.ru>
2021-09-08 09:06:33 +0500

    Finished Gatsby Tutorial Part 2.6

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hello there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage
+        alt="Andrey"
+        src="../images/Andrey.jpg"
+      />
     </Layout>
   )
 }
